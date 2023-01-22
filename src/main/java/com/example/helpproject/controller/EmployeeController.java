30f86017a2c25b5001084bce907ede11fb53de90
Maksym Rohachov <romax982@gmail.com>
2023-01-22 04:07:23 +0100

    Update the project

--- src/main/java/com/example/helpproject/controller/EmployeeController.java
+++ src/main/java/com/example/helpproject/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import com.example.helpproject.model.Employee;
 import com.example.helpproject.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
