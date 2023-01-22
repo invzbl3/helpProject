c2650eea5cadd66e1620c0a5049a2f22a5d4c38c
Maksym Rohachov <romax982@gmail.com>
2023-01-22 03:50:20 +0100

    Update the project

--- src/main/java/com/example/helpproject/repository/EmployeeRepository.java
+++ src/main/java/com/example/helpproject/repository/EmployeeRepository.java
@@ -2,13 +2,11 @@ package com.example.helpproject.repository;
 
 import com.example.helpproject.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author invzbl3 on 1/21/2023
  * @project helpProject
  */
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
