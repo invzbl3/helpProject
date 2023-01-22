2422ea3423a148f8268ff4e5590f1bbe763c7711
Maksym Rohachov <romax982@gmail.com>
2023-01-22 03:53:38 +0100

    Update the project

--- src/main/java/com/example/helpproject/model/Employee.java
+++ src/main/java/com/example/helpproject/model/Employee.java
@@ -1,6 +1,5 @@
 package com.example.helpproject.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
