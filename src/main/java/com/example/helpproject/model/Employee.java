d93ad1a9df213991b8f9a7d0ffbd4e6496fd32ce
Maksym Rohachov <romax982@gmail.com>
2023-01-22 03:45:43 +0100

    Update the project

--- src/main/java/com/example/helpproject/model/Employee.java
+++ src/main/java/com/example/helpproject/model/Employee.java
@@ -21,12 +21,9 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
     private String email;
 }
