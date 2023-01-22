92df2e13a0da793e1c0f5930db4ad2d1e58b6552
Maksym Rohachov <romax982@gmail.com>
2023-01-22 03:12:15 +0100

    Update the project

--- src/main/java/com/example/helpproject/controller/EmployeeController.java
+++ src/main/java/com/example/helpproject/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.helpproject.model.Employee;
 import com.example.helpproject.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,19 +12,16 @@ import org.springframework.web.bind.annotation.*;
  * @project helpProject
  */
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
-        super();
         this.employeeService = employeeService;
     }
 
-    // build REST API
-    //@RequestMapping("/api/employees")
-    //@RequestMapping(value = "/api/employees", method = RequestMethod.POST)
-    @PostMapping("/api/employees")
+    @PostMapping
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
         return new ResponseEntity<>
                 (employeeService.saveEmployee(employee), HttpStatus.CREATED);
